94626538886b9a5f72769726a645abf0b0294970
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-27 16:28:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -86,7 +86,15 @@ def game_hash
   :steals => 1,
   :blocks =>1,
   :slam_dunks => 0},
-  
+  {:player_name => "Kemba Walker",
+  :number => 33,
+  :shoe => 15,
+  :points => 6,
+  :rebounds => 12,
+  :assists => 12, 
+  :steals => 7,
+  :blocks => 5,
+  :slam_dunks => 12},
 ]
 }}
  hash
