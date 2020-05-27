f8409e56421d48850680144a14200c5d0534bd1b
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-27 14:08:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,17 @@
-# Write your code here!
+def game_hash
+ hash={
+:home => {:team_name =>"", :colors => [], :players => [
+  {:player_name},
+  {:number},
+  {:shoe},
+  {:points},
+  {:rebounds},
+  {:assists}, 
+  {:steals},
+  {:blocks},
+  {:slam_dunks}
+  ]},
+:away => 
+  
+ hash 
+end
