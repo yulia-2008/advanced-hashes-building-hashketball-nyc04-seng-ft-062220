d1fb918d0c7b21bb52e697e0f8b91f3345d8c801
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-27 14:15:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,17 +1,28 @@
 def game_hash
  hash={
-:home => {:team_name =>"", :colors => [], :players => [
-  {:player_name},
-  {:number},
-  {:shoe},
-  {:points},
-  {:rebounds},
-  {:assists}, 
-  {:steals},
-  {:blocks},
-  {:slam_dunks}
+:home => {:team_name =>"Brooklyn Nets", :colors => ["Black", "White"], :players => [
+  {:player_name =>},
+  {:number =>},
+  {:shoe =>},
+  {:points =>},
+  {:rebounds =>},
+  {:assists =>}, 
+  {:steals =>},
+  {:blocks =>},
+  {:slam_dunks =>}
   ]},
-:away => 
+:away => {:team_name =>"Charlotte Hornets", :colors => ["Turquoise", "Purple"], :players => [
+  {:player_name =>},
+  {:number =>},
+  {:shoe =>},
+  {:points =>},
+  {:rebounds =>},
+  {:assists =>}, 
+  {:steals=>},
+  {:blocks =>},
+  {:slam_dunks =>}
+  ]}
+ }
   
  hash 
 end
