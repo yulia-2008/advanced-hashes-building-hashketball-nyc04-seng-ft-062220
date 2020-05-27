2d94444a0a884cbdb7719821474ba4a94f67ee9d
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-27 16:08:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -31,8 +31,16 @@ def game_hash
   :steals => 2,
   :blocks => 7,
   :slam_dunks => 2},
-  {}
-  ]}}
+  {:player_name =>"Bismack Biyombo",
+  :number => 0,
+  :shoe => 16,
+  :points => 12,
+  :rebounds => 4,
+  :assists => 7, 
+  :steals => 22,
+  :blocks => 15,
+  :slam_dunks => 10}
+  ]}
    
  }
  hash
