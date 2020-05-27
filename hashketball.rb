af2686d6977de7748b2d038e3745844565555ad2
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-27 16:27:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -77,7 +77,15 @@ def game_hash
   :steals => 4,
   :blocks => 5,
   :slam_dunks =>5},
-  
+  {:player_name => "Ben Gordon",
+  :number => 8,
+  :shoe => 15,
+  :points => 33,
+  :rebounds => 3,
+  :assists => 2, 
+  :steals => 1,
+  :blocks => 12,
+  :slam_dunks => 7},
 ]
 }}
  hash
