9a95b34044db0fb87c1bfa88dd37826b05a30efb
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-27 16:24:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -37,7 +37,15 @@ def game_hash
   :steals => 3,
   :blocks => 8,
   :slam_dunks => 5},
-  
+  {:player_name => "Jason Terry",
+  :number => 31,
+  :shoe => 15,
+  :points => 19,
+  :rebounds => 2,
+  :assists => 2, 
+  :steals => 4,
+  :blocks => 11,
+  :slam_dunks => 1},
   
   ]},
 :away => {:team_name =>"Charlotte Hornets", :colors => ["Turquoise", "Purple"], 
